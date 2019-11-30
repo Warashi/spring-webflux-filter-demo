8180f796699b1ac3af205e186e29b7a34c243b32
Shinnosuke Sawada <6warashi9@gmail.com>
2019-11-30 09:33:55 +0900

    デモ用のcontroller実装

---
+++ src/main/kotlin/com/example/demo/DemoController.kt
@@ -0,0 +1,16 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+class DemoController {
+
+    @GetMapping("/")
+    fun demo(): Mono<String> {
+        return Mono.just("hello")
+            .flatMap {s -> Mono.subscriberContext()
+                .map { ctx -> "$s ${ctx.getOrDefault("ua", "world")}"}}
+    }
+}
